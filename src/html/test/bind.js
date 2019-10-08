71081b47ae8ebe27d674f3bd85831c294fcd44e2
derguo <wang2003225@qq.com>
2019-10-08 15:24:19 +0800

    add bind test

---
+++ src/html/test/bind.js
@@ -0,0 +1,15 @@
+var obj = {
+    a:10,
+    f:func.bind(null),
+    f1:function(){
+        console.log(this.toString());
+    }
+}
+
+function func(){
+    console.log(this.toString());
+}
+
+obj.f();
+
+obj.f1.bind(null)();
