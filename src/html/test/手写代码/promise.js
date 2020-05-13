5ae6c83b923472e5a6a837a191810bbdb74f38f2
who <wang2003225@aliyun.com>
2020-05-14 00:31:42 +0800

    完善了手写Promise

--- src/html/test/手写代码/promise.js
+++ src/html/test/手写代码/promise.js
@@ -4,7 +4,7 @@
  * @Author: wang zhiguo
  * @Date: 2020-05-03 21:42:27
  * @LastEditors: wang zhiguo
- * @LastEditTime: 2020-05-10 21:42:13
+ * @LastEditTime: 2020-05-14 00:30:48
  */
 /*
  * 1.有三个状态pending、fulfilled、rejected
@@ -18,73 +18,91 @@
  *   ③promise内部报错不会对外抛出
  * 7.（本手写Promise在执行上是错的）执行顺序上，注册的回调是分别被插入微任务的，也就是说注册一个回调就形成一个微任务，不是注册的回调统一在一个微任务中一起执行。模拟这个MyPromise实现的是错误的
  *    也就是说分两种情况，如果在pending状态下注册的，当改变状态的时候，这些回调都会一起执行，如果在其他状态注册的，不同的promise注册按照注册顺序执行。
+ * ！！！如上第七点已经得到修正，目前针对于第七点，处理方式和原生Promise机制是一样的
  */
 
 module.exports = (function () {
+  var con = 0;
   return function MyPromise(executor) {
+    var c = ++con;
     // pending、fulfilled、rejected
+    const PENDING = "pending";
+    const FULFILLED = "fulfilled";
+    const REJECTED = "rejected";
     var value;
-    var state = "pending";
+    var state = PENDING;
 
     var onQueue = [];
-    var inTaskOrNot;
+
+    var lastp;
 
     function change(s, v) {
-      if (state === "pending") {
+      if (state === PENDING) {
         value = v;
         state = s;
-        insertTask();
+        if (!onQueue.length && state === REJECTED) {
+          /* 当没有注册回调函数，而且状态为rejected，则在任务中放入一个提示任务。
+          如果在一个运行的这个宏任务中注册了回调，则在run中提示任务被移除 */
+          lastp = setTimeout(() => {
+            console.error("在MyPromise里，需要注册一个处理错误的回调");
+          }, 0);
+        }
+        onQueue.length && run();
       }
     }
 
     function run() {
-      if (state === "rejected" && onQueue.length === 0) {
-        console.error(
-          "在MyPromise里，需要注册一个处理异常情况的回调函数！！！"
-        );
-      }
-      while (onQueue.length) {
-        let onObj = onQueue.shift();
-        if (onObj[state].on) {
-          try {
-            let returnvalue = onObj[state].on(value);
-            if (returnvalue instanceof MyPromise) {
-              returnvalue.then(onObj["fulfilled"].next, onObj["rejected"].next);
-            } else {
-              onObj["fulfilled"].next(returnvalue);
-            }
-          } catch (error) {
-            onObj["rejected"].next(error);
-          }
-        } else {
-          onObj[state].next(value);
-        }
-      }
-    }
-
-    function insertTask(params) {
-      if (state === "pending") {
+      if (state === PENDING) {
         return;
       }
-      clearTimeout(inTaskOrNot);
-      inTaskOrNot = setTimeout(run, 0);
+
+      while (onQueue.length) {
+        /* 提示任务被移除，
+        在这里只要注册了回调，上面的提示任务都会被移除，
+        主要目的是把提示任务交给最后一个MyPromise去提示。
+        原因是只要调用了register（也就是then或者catch）下面的代码会把状态往下传 
+        如果这里判断相应状态移除提示任务，那么如果相应的状态回调为undefined，则就会保留提示，
+        下面的代码又把状态传下去了，结果提示任务就提示两次
+        并且如果注册多个回调，就会出现状态传递的多个分支。还要加以判断，麻烦没必要
+        反正最后总会有个没有注册回调的MyPromise，
+        把提示都抛给最后那个MyPromise处理，是最好的，也和原生Promise机制一样
+        */
+        clearTimeout(lastp);
+        let onObj = onQueue.shift();
+        setTimeout(() => {
+          if (onObj[state].on) {
+            try {
+              let returnvalue = onObj[state].on(value);
+              if (returnvalue instanceof MyPromise) {
+                returnvalue.then(onObj[FULFILLED].next, onObj[REJECTED].next);
+              } else {
+                onObj[FULFILLED].next(returnvalue);
+              }
+            } catch (error) {
+              onObj[REJECTED].next(error);
+            }
+          } else {
+            onObj[state].next(value);
+          }
+        }, 0);
+      }
     }
 
     function register(onFulfilled, onRejected) {
       var nextPromise = new MyPromise(function (resolve, reject) {
         onQueue.push({
-          fulfilled: { on: onFulfilled, next: resolve },
-          rejected: { on: onRejected, next: reject },
+          [FULFILLED]: { on: onFulfilled, next: resolve },
+          [REJECTED]: { on: onRejected, next: reject },
         });
       });
 
-      insertTask();
+      run();
       return nextPromise;
     }
 
-    var resolve = change.bind(this, "fulfilled");
+    var resolve = change.bind(this, FULFILLED);
 
-    var reject = change.bind(this, "rejected");
+    var reject = change.bind(this, REJECTED);
 
     this.then = register.bind(this);
 
--- src/html/test/手写代码/test.js
+++ src/html/test/手写代码/test.js
@@ -4,57 +4,57 @@
  * @Author: wang zhiguo
  * @Date: 2020-05-04 12:05:29
  * @LastEditors: wang zhiguo
- * @LastEditTime: 2020-05-10 21:27:53
+ * @LastEditTime: 2020-05-13 21:00:45
  */
 let MyPromise = require('./promise')
-// let p = new MyPromise(function (resolve, reject) {
-//     // resolve("ok0")
-//     setTimeout(() => {
-//         resolve("ok0")
-//     }, 3000);
-// }).then((value) => {
-//     console.log(value)
-//     return "ok1"
-// }).then(value => {
-//     console.log(value)
-//     return "ok2"
+let p = new MyPromise(function (resolve, reject) {
+    // resolve("ok0")
+    setTimeout(() => {
+        resolve("ok0")
+    }, 3000);
+}).then((value) => {
+    console.log(value)
+    return "ok1"
+}).then(value => {
+    console.log(value)
+    return "ok2"
+})
+
+p.then(value => {
+    console.log(value)
+})
+
+p.then(value => {
+    console.log(value)
+    return new MyPromise(function (resolve, reject) {
+        reject("ok3")
+        // setTimeout(() => {
+        //     reject("ok3")
+        // }, 3000);
+    })
+}).then(value => {
+    console.log(value)
+})
+
+// let p1 = new MyPromise((resolve, reject) => {
+//     resolve('p1:hahaha')
+//     // reject('yingyingying')
 // })
 
-// p.then(value => {
-//     console.log(value)
+// let p2 = new MyPromise((resolve, reject) => {
+//     resolve('p2:hahaha')
+//     // reject('yingyingying')
 // })
 
-// p.then(value => {
-//     console.log(value)
-//     return new MyPromise(function (resolve, reject) {
-//         reject("ok3")
-//         // setTimeout(() => {
-//         //     reject("ok3")
-//         // }, 3000);
-//     })
-// }).then(value => {
-//     console.log(value)
+// p1.then(value => {
+//     console.log(1, value)
 // })
-
-let p1 = new MyPromise((resolve, reject) => {
-    resolve('p1:hahaha')
-    // reject('yingyingying')
-})
-
-let p2 = new MyPromise((resolve, reject) => {
-    resolve('p2:hahaha')
-    // reject('yingyingying')
-})
-
-p1.then(value => {
-    console.log(1, value)
-})
-p2.then(value => {
-    console.log(2, value)
-})
-p1.then(value => {
-    console.log(1, value)
-})
+// p2.then(value => {
+//     console.log(2, value)
+// })
+// p1.then(value => {
+//     console.log(1, value)
+// })
 
 
 
